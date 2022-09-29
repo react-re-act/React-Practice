a0b029d3d6a2c198b4ecfe6dcca95bf050269151
lee juyoung <88328887+phnml1@users.noreply.github.com>
2022-09-30 01:00:05 +0900

    1주차 진도
    1주차 진도 까지 공부한 것 정리 및 코드도 입력

--- leejuyoung/src/App.js
+++ leejuyoung/src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  let [date, b] = useState(['2월 17일 발행','2월 17일 발행','2월 17일 발행']);
+  let [글제목, 글제목변경] = useState(['남자 코트추천','강남 우동맛집', '파이썬독학']); //destructuring 변수생성 문법
+  let [따봉,따봉변경] = useState(0); //오른쪽은 state변경함수
+  //자동렌더링
+  //document.queryselecor('h4').innerHtml = post;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>ReactBlog</h4> 
+      </div>
+      <button onClick = {()=> {
+        let copy = [...글제목];
+        copy[0] = '여자코트추천';
+        글제목변경(copy);
+      }}>글제목변경</button>
+      <button onClick={()=>{
+        let copy=[...글제목];
+        copy.sort(); //array정렬함수
+        글제목변경(copy);//변경함수에 변경한 독립적카피본 copy를 넣음
+      }}>가나다순정렬</button>
+      <div className='list'>
+        <h4>{ 글제목[0]}<span onClick={() => {따봉변경(따봉+1)} }>👍</span> {따봉} </h4> 
+        <p>{date[0]}</p>
+      </div>
+      <div className='list'>
+        <h4>{ 글제목[1] }</h4>
+        <p>{date[1]}</p>
+      </div>
+      <div className='list'>
+        <h4>{글제목[2]}</h4>
+        <p>{date[2]}</p>
+      </div>
     </div>
   );
 }
 
 export default App;
+
