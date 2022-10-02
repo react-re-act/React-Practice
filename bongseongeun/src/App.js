a6fbd9c643f60afeca576ab19bd78f4f85ee8d4d
봉성은 <bongfam2@naver.com>
2022-10-03 03:39:10 +0900

    리액트 PROPS

--- bongseongeun/src/App.js
+++ bongseongeun/src/App.js
@@ -1,18 +1,22 @@
 /* eslint-disable */
 import logo from './logo.svg';
 import './App.css';
-import{ useState } from 'react';
+import { useState } from 'react';
 function App() {
-
-  let post='강남 우동 맛집';
+  let post = '강남 우동 맛집';
   // let [코트, a]=useState('남자 코트 추천')
   // let [우동, d]=useState('강남 우동맛집')
   // let [파이썬, c]=useState('파이썬독학')
-  let [글제목, 글제목변경]=useState(['남자코트 추천', '강남 우동 맛집', '파이썬독학'])
-  let[따봉, 따봉변경]=useState([0,0,0]);
+  let [글제목, 글제목변경] = useState([
+    '남자코트 추천',
+    '강남 우동 맛집',
+    '파이썬독학',
+  ]);
+  let [title, setTitle] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
   // let[따봉1, 따봉변경1]=useState(0);
   // let[따봉2, 따봉변경2]=useState(0);
-  let[modal, setModal]=useState(false)
+  let [modal, setModal] = useState(false);
 
   // [1, 2, 3].map(function(){//array 자료 갯수만큼 함수 안의 코드 반복
   //   //function(a)
@@ -21,32 +25,40 @@ function App() {
 
   // })
 
-function 함수(){
-    따봉변경(따봉+1)   
-}
+  function 함수() {
+    따봉변경(따봉 + 1);
+  }
 
-function ascending ( a , b ) {return ( a < b ) ? -1 : ( a == b ) ? 0 : 1;        }
+  function ascending(a, b) {
+    return a < b ? -1 : a == b ? 0 : 1;
+  }
   return (
-    <div className="App">
-     <div className="black-nav">
-      <h4>ReactBlog</h4>
-     </div>
+    <div className='App'>
+      <div className='black-nav'>
+        <h4>ReactBlog</h4>
+      </div>
 
-    <button onClick={()=>{
-      let copy=[...글제목]
-      copy.sort()
-      글제목변경(copy)
-    }}>가나다순정렬
-    </button>
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.sort();
+          글제목변경(copy);
+        }}
+      >
+        가나다순정렬
+      </button>
 
-     <button onClick={()=>{
-        let copy =[...글제목]
-        copy[0]='여자코트 추천'
-        글제목변경(copy)
-      }}>글수정
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy[0] = '여자코트 추천';
+          글제목변경(copy);
+        }}
+      >
+        글수정
       </button>
       <div></div>
-     {/* <div className="list">
+      {/* <div className="list">
       <h4>{글제목[0]}<span onClick={함수}>👍</span> {따봉} </h4>
       <p>2월 17일 발행</p>
      </div>
@@ -58,37 +70,52 @@ function ascending ( a , b ) {return ( a < b ) ? -1 : ( a == b ) ? 0 : 1;       
       <h4 onClick={()=>{modal==true? setModal(false):setModal(true)}} >{글제목[2]}</h4>
       <p>2월 17일 발행</p>
      </div> */}
-      {
-        modal == true ? <Modal></Modal> : null
-      }
 
-      {
-        글제목.map(function(a, i){
-          return (
-          <div className="list">
-          <h4>{글제목[i]}<span onClick={()=>{
-            let copy=[...따봉]
-            copy[i]=copy[i]+1
-            따봉변경(copy)
-          }}>👍</span> {따봉[i]} </h4>
-          <p>2월 17일 발행</p>
-          </div>)
-        })
-      }
-
-
+      {글제목.map(function (a, i) {
+        return (
+          <div className='list'>
+            <h4
+              onClick={() => {
+                setModal(true);
+                setTitle(i);
+              }}
+            >
+              {글제목[i]}
+              <span
+                onClick={() => {
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy);
+                }}
+              >
+                👍
+              </span>{' '}
+              {따봉[i]}{' '}
+            </h4>
+            <p>2월 17일 발행</p>
+          </div>
+        );
+      })}
+      {modal == true ? (
+        <Modal title={title} 글제목변경={글제목변경} 글제목={글제목}></Modal>
+      ) : null}
     </div>
   );
 }
-
-function Modal(){//함수  축약
-  return(
-    <div className="modal">
-    <h4>제목</h4>
-    <p>날짜</p>
-    <p>상세내용</p>
-   </div>
-  )
+// 동적 UI만드는 3STEP
+// 1. html css로 미리 디자인
+// 2. 현재 상태를 state에 저장
+// 3 state에 따라 UI가 어떻게 보일지 작성
+function Modal(props) {
+  //함수  축약
+  return (
+    <div className='modal'>
+      <h4>{props.글제목[props.title]}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+      <button>글수정</button>
+    </div>
+  );
 }
 
 export default App;
