bb7ac39881f2480b2aa7485e7d235c892ddfbc99
lee juyoung <88328887+phnml1@users.noreply.github.com>
2022-10-05 16:52:07 +0900

    props까지^^

--- leejuyoung/src/App.js
+++ leejuyoung/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 function App() {
   
@@ -8,6 +8,7 @@ function App() {
   let [따봉,따봉변경] = useState([0,0,0]); //오른쪽은 state변경함수
   let [modal,setModal] = useState(false);
   let [title,setTitle] = useState(0);
+  let [입력값,입력값변경] = useState('');
 
   //자동렌더링
   return(
@@ -43,20 +44,39 @@ function App() {
               따봉변경(copy);
             } }>👍</span> {따봉[i]}</h4>
             <p>2월 17일 발행</p>
+            <button onClick={()=>{
+              let copy = [...글제목]
+              copy.splice(i,1)
+              글제목변경(copy)
+            }}>삭제</button>
           </div>
           )  
         })
         
       }
       <div>
-      <button onClick={()=>{setTitle(0)}}>글제목0</button>
-      <button onClick={()=>{setTitle(1)}}>글제목1</button>
-      <button onClick={()=>{setTitle(2)}}>글제목2</button>
+      {
+      글제목.map(function (a,i){
+        return(
+          <button onClick={() => {
+            setTitle(i)
+          }}>글제목{i}</button>
+        )
+      
+        })
+      }
       </div>
+      
+      <input onChange = {(e) => { 입력값변경(e.target.value) }}/>
+      <button onClick = {() =>{
+        let copy = [...글제목]
+        copy.unshift(입력값)
+        글제목변경(copy)
+      }}>글발행</button>
+
       {
       modal == true ?<Modal title = {title} 글제목 = {글제목} 글제목변경 = {글제목변경}/>: null
-      }
-      
+      } 
       </>
   )
 }
@@ -72,6 +92,24 @@ function Modal(props) {
         }}>글수정</button>
       </div>
   )
+}
+class Modal2 extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      name : "kim",
+      age :20
+    }
+  }
+  render() {
+    return(
+      <div>안녕 {this.state.age}
+      <button onClick={()=>{
+        this.setState({age:21})
+      }}>버튼</button>
+      </div>
+    )
+  }
 }
 export default App;
 
